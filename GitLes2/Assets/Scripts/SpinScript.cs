cdd5b197081e1275bf460d4781cc4963a5df3925
Mana <manakouwenberg@gmail.com>
2020-02-27 11:26:27 +0100

    SC-0001 -Added a spinning cube to the samplescene

---
+++ GitLes2/Assets/Scripts/SpinScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour
+{
+    #region Serialized
+    [SerializeField]
+    [Range(-5.0f, 5.0f)]
+    float spinSpeed = 1.0f;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y+spinSpeed, transform.rotation.eulerAngles.z + spinSpeed);
+    }
+}
